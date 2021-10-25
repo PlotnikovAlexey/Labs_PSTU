ee040a790779c0099972b32e81adad4bae7e6a2c
Alexey Plotnikov <92264296+Fast-Rush@users.noreply.github.com>
2021-10-25 19:36:49 +0500

    Игра на угадывание

---
+++ 26.10.21_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+
+int random(int f, int s)
+{
+	srand(time(NULL));
+	if (f > 0) return f + rand() % (s - f);
+	else return f + rand() % (abs(f) + s);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int attempt;
+	int number = random(1, 100);
+	cout << "Попробуйте угадать число. У вас 7 попыток:" << endl;
+	for (int i = 1; i <= 7; i++) {
+		cout << i << "-я попытка" << endl;
+		cin >> attempt;
+		if (number != attempt) {
+			if (attempt > number)
+				cout << "Нужно число меньше, чем " << attempt << endl;
+			else
+				cout << "Нужно число больше, чем " << attempt << endl;
+		}
+		else {
+			cout << "Вы выиграли " << attempt << endl;
+			return 0;
+		}
+	}
+	cout << "Вы проиграли" << endl << "Число:" << number;
+	return 0;
+}
