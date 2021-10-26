d66895a3f277fa60acf6a4987f9aba4bea9bc251
Alexey Plotnikov <92264296+Fast-Rush@users.noreply.github.com>
2021-10-26 16:47:33 +0500

    Update 26.10.21_2.cpp

--- 26.10.21_2.cpp
+++ 26.10.21_2.cpp
@@ -1,21 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
 #include <clocale>
 #include <ctime>
 
 using namespace std;
 
-int random(int f, int s)
-{
-	srand(time(NULL));
-	if (f > 0) return f + rand() % (s - f);
-	else return f + rand() % (abs(f) + s);
-}
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int attempt;
-	int number = random(1, 100);
+	int number = rand()%100+1;
 	cout << "Попробуйте угадать число. У вас 7 попыток:" << endl;
 	for (int i = 1; i <= 7; i++) {
 		cout << i << "-я попытка" << endl;
